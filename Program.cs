5f981547f1c241d6e1a358117d7aecee301c03d0
WiZaRd <wduck17@gmail.com>
2024-09-20 17:01:56 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using lab4_1;
+
+using(HelloappContext db = new HelloappContext())
+{
+    List<User> users = db.Users.ToList();
+    Console.WriteLine("users:");
+    foreach (User user in users)
+    {
+        Console.WriteLine($"{user.Id}.{user.Name} - {user.Age}");
+    }
+}
---
+++ User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lab4_1;
+
+public partial class User
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public int Age { get; set; }
+}
