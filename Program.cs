87f107a3ccfd18e580de84e1bb9dcc2307917bf1
WiZaRd <wduck17@gmail.com>
2024-09-20 20:45:49 +0300

    пушв мастер

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@
 
 using(HelloappContext db = new HelloappContext())
 {
+    bool isA = await db.Database.CanConnectAsync();
+    Console.WriteLine(isA);
     List<User> users = db.Users.ToList();
     Console.WriteLine("users:");
     foreach (User user in users)
